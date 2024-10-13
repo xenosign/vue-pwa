bcb43765f693d10b6f055bacc1ad9455aedabe04
kdtTetz <xenosign@naver.com>
2024-10-14 00:06:05 +0900

    241014 fcm 테스트 ing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,7 +23,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png',
+    icon: '/pwa-192x192.png',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
