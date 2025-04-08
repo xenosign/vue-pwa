f78f4c0c7347317fcbeab376be329e45e9185bdf
kdtTetz <xenosign@naver.com>
2025-04-08 20:08:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -38,7 +37,6 @@ export default defineConfig({
       workbox: {
         // Firebase FCM을 위한 서비스 워커 추가
         importScripts: ['/public/firebase-messaging-sw.js'],
-        // 기타 설정
       },
     }),
   ],
